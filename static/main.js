ac20b4b3fe1308bb6b56679e1f8418f3b0a1b19e
Nickolay Ponomarev <asqueella@gmail.com>
2016-05-01 04:38:20 +0300

    Switch to the Fetch API (instead of using the sync XHR).
    - Displays a better error when communicating with the readable-proxy server (e.g. when it is not started)
    - Only loads the original page in an iframe after we know readability ran successfully

--- static/main.js
+++ static/main.js
@@ -5,19 +5,17 @@
 
   function injectReadableContents(params, target) {
     q("#error-container").classList.add("hide");
-    var req = new XMLHttpRequest();
+
     var apiUrl = [
       "/api/get?sanitize=" + (params.sanitize ? "yes" : "no"),
       "url=" + encodeURIComponent(params.url),
       "userAgent=" + encodeURIComponent(params.userAgent),
       "phantomJSDebug=" + params.phantomJSDebug
     ].join("&");
-    req.open("GET", apiUrl, false);
-    req.send(null);
-    var jsonResponse = JSON.parse(req.responseText);
-    if (jsonResponse.error) {
-      q("#error").textContent = jsonResponse.error.message;
-      q("#error-container").classList.remove("hide");
+
+    return new Promise(function(resolve, reject) {
+      q("#submit-btn").disabled = true;
+      q("#error").textContent = "";
       q("#readerable").textContent = "";
       q("#title").textContent = "";
       q("#byline").textContent = "";
@@ -25,32 +23,53 @@
       q("#dir").textContent = "";
       q("#excerpt").textContent = "";
       q("#logs").value = "";
+      q("#stderr").textContent = "";
       target.contentDocument.body.innerHTML = "";
-    } else {
-      q("#error").textContent = "";
-      q("#readerable").textContent = jsonResponse.isProbablyReaderable;
-      q("#title").textContent = jsonResponse.title;
-      q("#byline").textContent = jsonResponse.byline;
-      q("#length").textContent = jsonResponse.length;
-      q("#dir").textContent = jsonResponse.dir;
-      q("#excerpt").textContent = jsonResponse.excerpt;
-      q("#logs").value = (jsonResponse.consoleLogs || []).join("\n");
-      target.contentDocument.body.innerHTML = jsonResponse.content;
-    }
-    q("#stderr").textContent = jsonResponse.stderr || "<stderr is empty>";
+
+      fetch(apiUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(jsonResponse) {
+          q("#stderr").textContent = jsonResponse.stderr || "<stderr is empty>";
+          if (jsonResponse.error) {
+            throw jsonResponse.error;
+          } else {
+            q("#readerable").textContent = jsonResponse.isProbablyReaderable;
+            q("#title").textContent = jsonResponse.title;
+            q("#byline").textContent = jsonResponse.byline;
+            q("#length").textContent = jsonResponse.length;
+            q("#dir").textContent = jsonResponse.dir;
+            q("#excerpt").textContent = jsonResponse.excerpt;
+            q("#logs").value = (jsonResponse.consoleLogs || []).join("\n");
+            target.contentDocument.body.innerHTML = jsonResponse.content;
+          }
+          q("#submit-btn").disabled = false;
+          resolve(jsonResponse);
+        })
+        .catch(function (reason) {
+          q("#submit-btn").disabled = false;
+          q("#error").textContent = reason.message;
+          q("#error-container").classList.remove("hide");
+          reject(reason);
+        });
+    });
   }
 
   function init() {
     q("form").addEventListener("submit", function(event) {
       event.preventDefault();
       var url = q("#url").value;
-      q("#source").src = url;
+      q("#source").src = "";
       injectReadableContents({
-        url: url,
-        sanitize: q("#sanitize").checked,
-        phantomJSDebug: q("#phantomJSDebug").checked,
-        userAgent: q("#userAgent").value
-      }, q("#target"));
+          url: url,
+          sanitize: q("#sanitize").checked,
+          phantomJSDebug: q("#phantomJSDebug").checked,
+          userAgent: q("#userAgent").value
+        }, q("#target"))
+      .then(function() {
+        q("#source").src = url;
+      });
     });
   }
 
